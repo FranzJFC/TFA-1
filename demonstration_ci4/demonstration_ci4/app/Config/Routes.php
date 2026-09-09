e73fae2116f9cecbd19e9b2e5170c0e8c74db9e0
FranzJFC <caburaojovs@gmail.com>
2026-09-09 20:16:02 +0800

    Redirect broken Render path

--- demonstration_ci4/demonstration_ci4/app/Config/Routes.php
+++ demonstration_ci4/demonstration_ci4/app/Config/Routes.php
@@ -11,3 +11,5 @@ $routes->get('/customers', 'Customers::index');
 $routes->get('/users', 'Users::index');
 $routes->addRedirect('/homepage', '/');
 $routes->addRedirect('/homepage/(:any)', '/');
+$routes->addRedirect('/var/www/html/public/index.php/public', '/');
+$routes->addRedirect('/var/www/html/public/index.php/public/(:any)', '/$1');
